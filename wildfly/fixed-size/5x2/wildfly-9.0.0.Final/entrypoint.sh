da2dbe188bcd9ed87da06d3708dbfccf1a460a2d
Harald Pehl <harald.pehl@gmail.com>
2015-07-06 13:16:39 +0200

    Dockerfiles for EAP 6.3.0.GA domain

--- wildfly/fixed-size/5x2/wildfly-9.0.0.Final/entrypoint.sh
+++ wildfly/fixed-size/5x2/wildfly-9.0.0.Final/entrypoint.sh
@@ -8,13 +8,7 @@ then
     sed -i "s/@WILDFLY_MANAGEMENT_PASSWORD@/`echo ${WILDFLY_MANAGEMENT_PASSWORD} | base64`/" ${JBOSS_HOME}/domain/configuration/host-slave.xml
 fi
 
-if [[ ! -z "${WILDFLY_APPLICATION_USER}" ]] && [[ ! -z "${WILDFLY_APPLICATION_PASSWORD}" ]]
-then
-    ${JBOSS_HOME}/bin/add-user.sh --silent -e -a -u ${WILDFLY_APPLICATION_USER} -p ${WILDFLY_APPLICATION_PASSWORD}
-fi
-
 # Unset the temporary env variables
 unset ${WILDFLY_MANAGEMENT_USER} ${WILDFLY_MANAGEMENT_PASSWORD}
-unset ${WILDFLY_APPLICATION_USER} ${WILDFLY_APPLICATION_PASSWORD}
 
 exec ${JBOSS_HOME}/bin/domain.sh "$@"
